dadc308469860b6d87c755ad1a02810c51c2b658
Jli0423 <jli0423@DESKTOP-35FGBMU.localdomain>
2017-07-12 22:38:21 -0400

    Fixed config for heroku MLab

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development'){
   process.env.PORT= 3000;
-  process.env.MONGODB_URI= 'mongodb://localhost:27017/CDA-Students';
+  process.env.MONGODB_URI= 'mongodb://jli0423:Justin_199898li@ds064299.mlab.com:64299/heroku';
 }else if (env === 'test'){
   process.env.PORT = 3000;
   process.env.MONGODB_URI= 'mongodb://localhost:27017/CDA-StudentsTest';
