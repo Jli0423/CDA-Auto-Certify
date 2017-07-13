1c96a26fd275cc3b4942d470a5c81cbf9c120860
Jli0423 <jli0423@DESKTOP-35FGBMU.localdomain>
2017-07-12 22:42:05 -0400

    Fixed for mLab

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'localhost:27017/CDA-Students');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://jli0423:Justin_199898li@ds064299.mlab.com:64299/heroku');
 
 module.exports = {mongoose};
